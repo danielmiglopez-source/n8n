423d5ee5e8610c8eb4b0a788f5a5fb2841106db1
Danny Martini <danny@n8n.io>
2025-08-19 10:20:00 +0100

    refactor(core): Split `WorkflowExecute.runNode` into smaller methods (#17864)
    initial implementation of executing tools inside the engine
    
    remove debugger statement
    
    extract handling disabled nodes from runNode
    
    extract preparing connection input data creation from runNode
    
    extract error handling logic from runNode
    
    extract handleExecuteOnce from runNode
    
    extract node execution logic from runNode
    
    extract poll node logic from runNode
    
    extract trigger node logic from runNode
    
    extract declarative node logic from runNode
    
    remove eslint disable
    
    remove unused parameter
    
    add additional types
    
    extract setup and workflow checks from processRunExecutionData
    
    extract execution data assertion from processRunExecutionData
    
    group let bindings
    
    extract waitUntil handling from processRunExecutionData
    
    add id
    
    add notes
    
    wip: Make agent return actions
    
    execute sub nodes correctly
    
    build process improvements
    
    add helper function and reconstruct state of agent from tool run data
    
    add runIndex to ITaskMetadata instead of inferring it
    
    deduplicate node
    
    rename Foo to Request and re-implement helper function by keeping track
    of the tool executions in the engine
    
    Fix toolCallId passing
    
    remove helper function and pass the response directly to the execute
    function
    
    add new version for agent
    
    Fix other tool types
    
    Fix prod webhook execution with waiting in tools
    
    Unwrap logwrapper wrapped tools and set inputOverrides correctly
    
    Keep track of previous tool calls in agent
    
    Fix memory and some type errors
    
    changes from master
    
    Fix handling of vector store as tools in the engine
    
    clean up commented out code
    
    Fix: Handle fromAI expressions in webhook executions and readd inputOverrides if present
    
    Add testing node for LLMs
    
    Fix type errors
    
    Fix build for now
    
    Fix output format of wikipedia tool
    
    Add execute function to the ToolWorkflow and disable logging
    
    Make calculator work in new execution mode
    
    Correct response pormat on calculator execute
    
    Fix code tool execution in engine

--- packages/workflow/src/interfaces.ts
+++ packages/workflow/src/interfaces.ts
@@ -425,6 +425,8 @@ export interface IExecuteData {
 	metadata?: ITaskMetadata;
 	node: INode;
 	source: ITaskDataConnectionsSource | null;
+	// TODO: try to make this required
+	runIndex?: number;
 }
 
 export type IContextObject = {
@@ -972,6 +974,8 @@ export type IExecuteFunctions = ExecuteFunctions.GetNodeParameterFn &
 		getInputData(inputIndex?: number, connectionType?: NodeConnectionType): INodeExecutionData[];
 		getNodeInputs(): INodeInputConfiguration[];
 		getNodeOutputs(): INodeOutputConfiguration[];
+		getSubnodes(connectionType: NodeConnectionType): string[];
+		getRunIndex(): number;
 		putExecutionToWait(waitTill: Date): Promise<void>;
 		sendMessageToUI(message: any): void;
 		sendResponse(response: IExecuteResponsePromiseData): void;
@@ -1058,6 +1062,7 @@ export type ISupplyDataFunctions = ExecuteFunctions.GetNodeParameterFn &
 		evaluateExpression(expression: string, itemIndex: number): NodeParameterValueType;
 		getWorkflowDataProxy(itemIndex: number): IWorkflowDataProxyData;
 		getExecutionCancelSignal(): AbortSignal | undefined;
+		getSubnodes(connectionType: NodeConnectionType): string[];
 		onExecutionCancellation(handler: () => unknown): void;
 		logAiEvent(eventName: AiEvent, msg?: string): void;
 		cloneWith(replacements: {
@@ -1699,7 +1704,7 @@ type NodeOutput = INodeExecutionData[][] | NodeExecutionWithMetadata[][] | null;
 export interface INodeType {
 	description: INodeTypeDescription;
 	supplyData?(this: ISupplyDataFunctions, itemIndex: number): Promise<SupplyData>;
-	execute?(this: IExecuteFunctions): Promise<NodeOutput>;
+	execute?(this: IExecuteFunctions, responses?: SubNodeExecutionResult[]): Promise<NodeOutput>;
 	/**
 	 * A function called when a node receives a chat message. Allows it to react
 	 * to the message before it gets executed.
@@ -1755,13 +1760,35 @@ export interface INodeType {
 	};
 }
 
+type ExecutionNodeAction<T extends unknown> = {
+	actionType: 'ExecutionNodeAction';
+	nodeName: string;
+	input: IDataObject;
+	// input: string | Record<string, unknown>;
+	// input: INodeExecutionData[][] | null | undefined;
+	type: NodeConnectionType;
+	id: string;
+	metadata: T;
+};
+type Action<T = unknown> = ExecutionNodeAction<T>;
+export type Request<T = unknown> = {
+	actions: Array<Action<T>>;
+};
+export type SubNodeExecutionResult<T = unknown> = {
+	action: ExecutionNodeAction<T>;
+	data: ITaskData;
+};
+
 /**
  * This class serves as the base for all nodes using the new context API
  * having this as a class enables us to identify these instances at runtime
  */
 export abstract class Node {
 	abstract description: INodeTypeDescription;
-	execute?(context: IExecuteFunctions): Promise<INodeExecutionData[][]>;
+	execute?(
+		context: IExecuteFunctions,
+		response?: SubNodeExecutionResult[],
+	): Promise<INodeExecutionData[][] | Request<unknown>>;
 	webhook?(context: IWebhookFunctions): Promise<IWebhookResponseData>;
 	poll?(context: IPollFunctions): Promise<INodeExecutionData[][] | null>;
 }
@@ -2349,6 +2376,12 @@ export interface ITaskMetadata {
 	parentExecution?: RelatedExecution;
 	subExecution?: RelatedExecution;
 	subExecutionsCount?: number;
+	subNodeExecutionData?: Array<{
+		nodeName: string;
+		runIndex: number;
+		action: Request['actions'][number];
+		response?: unknown;
+	}>;
 }
 
 /** The data that gets returned when a node execution starts */
